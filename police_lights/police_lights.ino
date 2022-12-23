0a9ac0495b76c348936a6de67da09dc5298205df
Aleksbgbg <aleks.todorov.1337@gmail.com>
2022-12-23 19:23:58 +0000

    Enable police_lights in sequence for any number of kLights.

--- police_lights/police_lights.ino
+++ police_lights/police_lights.ino
@@ -1,29 +1,47 @@
+#define ON HIGH
+#define OFF LOW
+
 namespace {
 
-static constexpr uint32_t kSwitchDelay = 250;
+static constexpr uint8_t kLights = 4;
+static constexpr uint8_t kStartPin = 3;
+static constexpr uint8_t kMaxLight = kLights - 1;
+static constexpr uint32_t kSwitchDelay = 50;
 
-struct Led {
-  uint8_t pin;
-  uint8_t state;
-};
+uint8_t current_light = kMaxLight;
 
-Led Leds[] = {
-    {.pin = 3, .state = HIGH},
-    {.pin = 4, .state = LOW},
-};
+template <typename... T>
+using Action = void (*)(T...);
+
+void ForAllLights(Action<uint8_t> action) {
+  for (uint8_t light = 0; light < kLights; ++light) {
+    action(light);
+  }
+}
+
+uint8_t LightPin(uint8_t light_index) {
+  return kStartPin + light_index;
+}
+
+void SetCurrentLight(uint8_t state) {
+  digitalWrite(LightPin(current_light), state);
+}
+
+uint8_t SwitchToNextLight() {
+  current_light = (current_light + 1) % kLights;
+}
 
 }  // namespace
 
 void setup() {
-  for (const Led& led : Leds) {
-    pinMode(led.pin, OUTPUT);
-  }
+  ForAllLights([](uint8_t light) {
+    pinMode(LightPin(light), OUTPUT);
+  });
 }
 
 void loop() {
-  for (Led& led : Leds) {
-    led.state = !led.state;
-    digitalWrite(led.pin, led.state);
-  }
+  SetCurrentLight(OFF);
+  SwitchToNextLight();
+  SetCurrentLight(ON);
   delay(kSwitchDelay);
 }
